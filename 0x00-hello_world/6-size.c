5c51c21f6e1d241a02ba98f5702c4b52edd41c17
Afeez Adedeji <afeezadedeji4@gmail.com>
2022-09-09 01:51:39 -0700

    size_of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char syschar;
+	int sysint;
+	long intlong;
+	long long intlonglong;
+	float sysfloat;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(syschar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(sysint));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(intlong));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(intlonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(sysfloat));
+
+	return (0);
+}
