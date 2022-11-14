db832323a850bec75c806d796b08b8a542f6ada5
Afeez Adedeji <afeezadedeji4@gmail.com>
2022-11-14 21:16:52 +0100

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
 
 /**
- *  * main - entry
- *   * @void: takes no args
- *    *
- *     * Return: 0 if successful
- *      */
+ * main - entry
+ * @void: takes no args
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
 		double i;
-			double end;
-				double t1;
-					double t2;
-						double nextTerm;
+		double end;
+		double t1;
+		double t2;
+		double nextTerm;
 
-							end = 98;
-								t1 = 1;
-									t2 = 2;
+		end = 98;
+		t1 = 1;
+		t2 = 2;
 
-										printf("%.0f, %.0f, ", t1, t2);
+		printf("%.0f, %.0f, ", t1, t2);
 
-											for (i = 0; i < end; i++)
-													{
-
-																nextTerm = t1 + t2;
-																		if (i == end - 1)
-																					{
-																									printf("%.0f\n", nextTerm);
-																											}
-																				else
-																							{
-																											printf("%.0f, ", nextTerm);
-																													}
-
-																						t1 = t2;
-																								t2 = nextTerm;
+		for (i = 0; i < end; i++)
+			{
+				nextTerm = t1 + t2;
+			if (i == (end - 1))
+					{
+						printf("%.0f\n", nextTerm);
+					}
+				else
+					{
+						printf("%.0f, ", nextTerm);
+						t1 = t2;
+						t2 = nextTerm;
 																									}
-												return (0);
+				return (0);
 }
