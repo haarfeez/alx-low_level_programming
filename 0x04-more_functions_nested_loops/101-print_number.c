50ae62f651324f107567d64cd0e2271d54c2f53f
Afeez Adedeji <afeezadedeji4@gmail.com>
2022-11-15 17:11:50 +0100

    a

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,24 +8,24 @@ nclude "main.h"
  * No return
  */
 
-void print_number(int n)
-{
-		unsigned int x = 0;
 
+#includnt n)
+{
+		unsigned int n1;
+		
 			if (n < 0)
 					{
-								n = n * -1;
-
-										x = n;
-												_putchar('-');
-													}
-				else
-						{
-									x = n;
-										}
-
-					if (x / 10)
-								print_number(x / 10);
-
-						_putchar((x % 10) + '0');
+								n1 = -n;
+										_putchar('-');
+											} else
+													{
+																n1 = n;
+																	}
+											
+												if (n1 / 10)
+														{
+																	print_number(n1 / 10);
+																		}
+												
+													_putchar((n1 % 10) + '0');
 }
